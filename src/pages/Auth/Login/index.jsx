79ef6c7eb7e8fedd2ead285e2dcf41f00a7f4ac6
Vitalii Zakharchuk <95skin95@gmail.com>
2021-06-12 16:26:32 +0300

    stage three

--- src/pages/Auth/Login/index.jsx
+++ src/pages/Auth/Login/index.jsx
@@ -2,17 +2,36 @@
 import React, {useState} from "react";
 import {useHistory} from 'react-router-dom';
 
+//Function
+import {authenticateUser} from '../../../sdk';
 
 //Form
 import {AuthLogin} from "./FormaLogin";
+
+//Style
+import {toast} from "react-toastify";
 
 const Login = () => {
     const [loading, setLoading] = useState(false)
     const history = useHistory();
 
-    const handleSubmit = (email, password) => {
-               console.log('email', email)
-               console.log('password', password)
+    const handleSubmit = async (email, password) => {
+        setLoading(true)
+
+        await authenticateUser(email, password)
+            .then(res => {
+                res.text().then(function(text) {
+                    toast.success(`Hello ${JSON.parse(text).email}`)
+                    history.push('/')
+                });
+            })
+            .catch(error => {
+                setLoading(false);
+
+                error.text().then(function(text) {
+                    toast.error(JSON.parse(text).message)
+                });
+            })
     }
 
     return (
